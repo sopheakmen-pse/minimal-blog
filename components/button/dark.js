13be1805e825059693ecca9b86a7ce5da7b2712e
sengkimin <sengin.pse@gmail.com>
2024-03-13 13:47:58 +0700

    button

---
+++ components/button/dark.js
@@ -0,0 +1,4 @@
+document.getElementById('dark').addEventListener('click',function(event){
+
+});
+
---
+++ components/navbar/index.js
@@ -0,0 +1,5 @@
+document.getElementById('btn'). addEventListener('click',function(event){
+    // console.log("I clicked the button!!!");
+    // alert("I clicked the button!!!");
+    // return true;
+  });
