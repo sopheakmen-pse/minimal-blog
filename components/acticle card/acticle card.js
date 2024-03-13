ae9461f656fe620a607ddc4d9c8df61f60f256b0
sengkimin <sengin.pse@gmail.com>
2024-03-13 16:43:13 +0700

    Development Tools

--- components/acticle card/acticle card.js
+++ components/acticle card/acticle card.js
@@ -199,22 +199,222 @@ const articleDailyTrends =[
 
 
 
+const articleDevelopmenttool =[
+  {
+    id: "id 7",
+    title: "Article7",
+    category: "Development tool",
+    primaryImageUrl: "https://orangematter.solarwinds.com/wp-content/uploads/2020/03/istock-1135521404.jpg",
+    secondaryImageUrl: "https://img.freepik.com/free-vector/1.jpg",
+    publishDate: "2024-03-01",
+    paragraphs: [
+      '1. Introduction:Welcome to the future of UI/UX design! As we dive into 2023, the design landscape is brimming with innovative tools that promise to revolutionize the way we create digital experiences. In this blog post, well explore some of the best UI/UX design tools that are setting the standard for creativity, collaboration, and efficiency in the industry.',
+      
+      '2. Sketch:Leading the pack is Sketch, a powerhouse in UI design known for its simplicity and versatility. With an array of features like nested symbols and shared libraries, Sketch empowers designers to craft pixel-perfect interfaces with ease. In 2023, Sketch remains a favorite among professionals for its intuitive workflow and robust plugin ecosystem',
+      
+      '3. Figma: Figma continues to gain traction as the go-to tool for collaborative design. Its cloud-based platform allows teams to work together in real-time, making feedback and iteration seamless. With features like auto-layout and interactive components, Figma is at the forefront of design innovation in 2023.',
+      
+      '4. Adobe XD:Adobe XD remains a powerhouse in the design world, offering a comprehensive suite of tools for both UI and UX design. With features like voice prototyping and responsive resize, Adobe XD empowers designers to create immersive experiences across devices. In 2023, Adobe XD continues to evolve, pushing the boundaries of whats possible in digital design.',
+      
+      '5. InVision Studio:InVision Studio is a game-changer for prototyping and animation, offering a rich set of tools for bringing designs to life. With features like advanced motion transitions and adaptive layouts, InVision Studio is a playground for designers looking to push the boundaries of creativity in 2023.',
+      
+      '6. Principle:Principle is a go-to tool for creating stunning animations and interactive prototypes. Its intuitive interface and powerful timeline make it easy to bring designs to life with realistic motion. In 2023, Principle remains a favorite among designers for its simplicity and versatility.',
+      
+     '7. Marvel: Marvel is a user-friendly prototyping tool that empowers designers to create interactive prototypes with ease. With features like hotspots and gestures, Marvel is perfect for quickly iterating on designs and gathering feedback. In 2023, Marvel continues to be a staple in the designers toolkit for its simplicity and accessibility.',
+      
+      '8. Zeplin:Zeplin is an essential tool for bridging the gap between design and development. Its seamless integration with design tools like Sketch and Figma makes it easy to generate specs and assets for developers. In 2023, Zeplin remains a must-have for teams looking to streamline their workflow and improve collaboration.',
+      
+     ' 9. Axure RP:Axure RP is a powerhouse for creating highly interactive prototypes with advanced functionality. Its robust set of features, including dynamic content and conditional logic, make it perfect for designing complex user experiences. In 2023, Axure RP continues to be a go-to tool for designers working on ambitious projects.',
+      
+      '10. Adobe Illustrator: Adobe Illustrator remains a staple in the designers toolkit for creating stunning vector graphics and illustrations. With features like artboards and responsive resizing, Illustrator is perfect for crafting scalable designs for any screen size. In 2023, Adobe Illustrator continues to be a go-to tool for designers looking to push the boundaries of creativity.',
+      
+      '11. Conclusion:As we journey further into the digital age, the tools we use play a crucial role in shaping the future of design. Whether youre creating pixel-perfect interfaces or immersive user experiences, the best UI/UX design tools of 2023 empower designers to bring their ideas to life with speed, precision, and creativity.',
+
+    ]
+  },
+
+
+  { 
+     id: "id 8",
+      title: "Article8",
+      category: "Development tool",
+      primaryImageUrl: "https://orangematter.solarwinds.com/wp-content/uploads/2020/03/istock-1135521404.jpg",
+      secondaryImageUrl: "https://img.freepik.com/free-vector/1.jpg",
+      publishDate: "2024-03-01",
+      paragraphs: [
+     
+    '1. Streamlined Collaboration:Zeplin enhances UI/UX design collaboration by bridging the gap between designers and developers. Its platform simplifies communication and fosters efficient handoff of design assets, ensuring a seamless workflow.',
+
+    '2. Design Handoff Made Easy:With Zeplin, designers can upload their designs directly from tools like Sketch or Figma, allowing developers to access assets effortlessly. This streamlined handoff process reduces miscommunication and accelerates development.',
+
+    '3. Automatic Style Guide Generation:Zeplin automates the generation of style guides based on uploaded design files. These guides include essential details like colors, fonts, and measurements, providing developers with a comprehensive reference for implementing the design accurately.',
+
+    '4. Code Snippet Generation:Developers can easily generate code snippets directly from Zeplin, speeding up the implementation of design elements. This feature ensures consistency between design and development, reducing the likelihood of errors and discrepancies.',
+
+   '5. Version Control:Zeplin offers robust version control features, allowing teams to track changes to the design over time. This ensures that everyone is working from the latest iteration, minimizing confusion and ensuring alignment throughout the design process.',
+
+   '6. Feedback Management:Zeplin provides tools for managing feedback directly within the platform, streamlining the feedback loop between designers and developers. This centralized feedback system promotes collaboration and iteration, resulting in better outcomes.',
+
+   '7. Integration with Development Platforms:Zeplin seamlessly integrates with popular development platforms like GitHub and GitLab, further enhancing collaboration between design and development teams. Developers can access design assets directly from their development environment, eliminating friction and improving workflow efficiency.',
+
+   '8. Real-Time Updates:With Zeplin, teams have access to real-time updates, ensuring that everyone is working from the latest version of the design. This promotes agility and responsiveness, allowing teams to adapt quickly to changes and iterate as needed.',
+
+   '9. Customizable Workspaces:Zeplin offers customizable workspaces tailored to the specific needs of each project. Whether working on a small-scale project or a large-scale endeavor, teams can organize and manage design assets effectively, maximizing productivity and collaboration.',
+
+   '10. Security and Privacy:Zeplin prioritizes security and privacy, ensuring that design files are protected at all times. With features like end-to-end encryption and granular access controls, teams can trust that their sensitive design assets are safe and secure.',
+
+   '11. Conclusion:In conclusion, Zeplin is a game-changer for UI/UX design collaboration, empowering teams to work together more efficiently and effectively. By streamlining the design handoff process, automating style guide generation, and providing robust version control and feedback management features, Zeplin facilitates seamless collaboration and drives successful design outcomes.',
+      ]
+  },
+
+  { 
+    id: "id 9",
+     title: "Article9",
+     category: "Development tool",
+     primaryImageUrl: "https://orangematter.solarwinds.com/wp-content/uploads/2020/03/istock-1135521404.jpg",
+     secondaryImageUrl: "https://img.freepik.com/free-vector/1.jpg",
+     publishDate: "2024-03-01",
+     paragraphs: [
+
+
+      '1.Introduction:Adobe XD stands out as a versatile tool for modern UI/UX designers, offering a comprehensive solution for creating interactive prototypes and designs.',
+      
+      '2.User-Friendly Interface:Adobe XD boasts an intuitive and user-friendly interface, making it accessible to designers of all skill levels.',
+      
+      '3.Powerful Features:With features like responsive resizing and auto-animate, Adobe XD empowers designers to create dynamic and responsive prototypes with ease.',
+      
+      '4.Cross-Platform Compatibility:Adobe XD is compatible across multiple platforms, allowing designers to seamlessly transition between desktop and mobile devices.',
+      
+      '5. Wireframing and Prototyping: From wireframing to prototyping, Adobe XD provides a seamless workflow for designing and iterating on user interfaces.',
+      
+      '6.Collaboration and Sharing: Adobe XD facilitates collaboration among team members with its cloud-based platform, enabling real-time editing and sharing of designs.',
+      
+      '7.Integration with Creative Cloud: As part of the Adobe Creative Cloud suite, Adobe XD seamlessly integrates with other Adobe products, enhancing the design process.',
+      
+      '8.Continuous Updates and Improvements:Adobe XD is regularly updated with new features and improvements, ensuring that designers have access to the latest tools and technologies.',
+      
+     '9.Community Support and Resources:The Adobe XD community offers a wealth of resources, including tutorials, forums, and plugins, to support designers in their creative endeavors.',
+      
+      '10.Versatility Across Projects:Whether designing websites, mobile apps, or other digital experiences, Adobe XD provides the flexibility and functionality needed to bring ideas to life.',
+      
+    '11.Conclusion:In conclusion, Adobe XD is a powerful and versatile tool that empowers designers to create exceptional user experiences across a variety of platforms. With its user-friendly interface, powerful features, and seamless integration, Adobe XD is a valuable asset for any UI/UX design project.',
+     ]
+ },
 
 
 
+ { 
+  id: "id 10",
+   title: "Article10",
+   category: "Development tool",
+   primaryImageUrl: "https://orangematter.solarwinds.com/wp-content/uploads/2020/03/istock-1135521404.jpg",
+   secondaryImageUrl: "https://img.freepik.com/free-vector/1.jpg",
+   publishDate: "2024-03-01",
+   paragraphs: [
+
+
+'1.Introduction:Framer is a dynamic prototyping tool that empowers designers to create interactive and engaging prototypes, making it a valuable asset for both beginners and experienced designers.',
+
+'2.User-Friendly Interface:Framer boasts a user-friendly interface that simplifies the prototyping process, making it accessible to designers of all skill levels.',
+
+'3. Code-Based Prototyping:Unlike many other prototyping tools, Framer utilizes a code-based approach, allowing designers to create prototypes using familiar coding languages like JavaScript.',
+
+'4.Powerful Animation Tools:Framer offers a wide range of powerful animation tools, enabling designers to create fluid transitions and interactive animations that bring their prototypes to life.',
+
+'5.Component-Based Design:With Framers component-based design system, designers can create reusable elements and UI components, streamlining the prototyping process and ensuring consistency across designs.',
+
+'6.Real-Time Preview:Framer provides a real-time preview feature that allows designers to see their prototypes in action as they build them, making it easy to iterate and refine their designs on the fly.',
+
+'7.Collaboration and Sharing:Framer facilitates collaboration among team members with its cloud-based platform, allowing designers to share prototypes and gather feedback from stakeholders in real-time.',
+
+'8.Interactive Components:Framer offers a variety of interactive components, such as buttons, sliders, and input fields, that designers can easily incorporate into their prototypes to create rich and immersive user experiences.',
+
+'9.Responsive Design:With Framers responsive design features, designers can create prototypes that adapt seamlessly to different screen sizes and devices, ensuring a consistent user experience across platforms.',
+
+'10.Extensive Documentation and Support:Framer provides extensive documentation, tutorials, and community support resources, making it easy for beginners to get started and learn the ins and outs of the tool.',
+
+'11. Conclusion:In conclusion, Framer is a powerful prototyping tool that offers a wealth of features and capabilities for designers looking to create interactive and engaging prototypes. With its user-friendly interface, code-based approach, and extensive set of tools, Framer is an invaluable resource for designers at every stage of the prototyping process.',
+
+
+   ]
+},
+
+{
+id: "id 11",
+title: "Article11",
+category: "Development tool",
+primaryImageUrl: "https://orangematter.solarwinds.com/wp-content/uploads/2020/03/istock-1135521404.jpg",
+secondaryImageUrl: "https://img.freepik.com/free-vector/1.jpg",
+publishDate: "2024-03-01",
+paragraphs: [
+ 
+
+'1.Introduction:Design handoff is a critical stage in the UI/UX design process, where designers transfer their designs to developers for implementation. Avocode simplifies this process by providing a seamless platform for collaboration between designers and developers.',
+
+'2.Import Designs:Avocode allows designers to import their designs from various design tools such as Sketch, Adobe XD, and Figma, ensuring compatibility across different platforms.',
+
+'3.Inspect Designs:Once imported, Avocode provides developers with a comprehensive set of tools to inspect designs, including measurements, colors, and fonts. This ensures that developers have all the information they need to accurately implement the design.',
+
+'4.Generate Code:Avocode generates code snippets automatically, making it easy for developers to translate designs into code. This streamlines the development process and reduces the risk of errors.',
+
+'5.Export Assets:Avocode allows designers to export assets directly from their designs, including images, icons, and SVGs. This saves time and ensures that developers have access to all the necessary assets.',
+
+'6.Version Control:Avocode offers version control features, allowing designers to track changes to their designs over time. This ensures that everyone is working from the latest version of the design and helps prevent conflicts during the development process.',
+
+'7.Collaboration Tools:Avocode provides collaboration tools that allow designers and developers to communicate and collaborate effectively. Features such as commenting and annotations make it easy to provide feedback and make revisions.',
+
+'8.Integration with Development Tools:Avocode integrates seamlessly with popular development tools such as GitHub and Bitbucket, allowing developers to access designs directly from their development environment.',
+
+'9.Security and Privacy:Avocode prioritizes security and privacy, ensuring that designs are protected at all times. With features such as end-to-end encryption and access controls, designers can trust that their designs are safe and secure.',
+
+'10.Cross-Platform Compatibility:Avocode is compatible across different platforms and devices, allowing designers and developers to collaborate from anywhere.',
+
+'11.Conclusion:In conclusion, Avocode is a powerful tool that simplifies the design handoff process, making it easy for designers and developers to collaborate effectively. With its comprehensive set of features, seamless integration with development tools, and commitment to security and privacy, Avocode is an invaluable asset for any design team.',
 
 
 
+]
+},
+
+{
+  id: "id 12",
+  title: "Article12",
+  category: "Development tool",
+  primaryImageUrl: "https://orangematter.solarwinds.com/wp-content/uploads/2020/03/istock-1135521404.jpg",
+  secondaryImageUrl: "https://img.freepik.com/free-vector/1.jpg",
+  publishDate: "2024-03-01",
+  paragraphs: [
+   
+  
+  
 
 
-
-
-
-
-
-
-
-
+   '1.Introduction:Collaborative design is at the core of successful projects, and InVision is a powerful platform that facilitates teamwork among designers, developers, and stakeholders.',
+  
+    '2.Real-time Collaboration:InVision enables real-time collaboration, allowing team members to work together on designs simultaneously, regardless of their location.',
+    
+    '3.Commenting and Feedback:With features like commenting and feedback, team members can provide input directly on designs, fostering communication and iteration.',
+    
+    '4.Version Control:InVision offers robust version control capabilities, allowing teams to track changes and revert to previous versions if needed, ensuring everyone works from the latest iteration.',
+    
+    '5.Prototyping and Interaction:InVisions prototyping tools enable designers to create interactive prototypes with ease, allowing stakeholders to experience the design in action and provide feedback.',
+    
+    '6.Design System Management:InVision supports design system management, making it easy to maintain consistency across projects and ensure adherence to brand guidelines.',
+    
+    '7.Integration with Design Tools: InVision integrates seamlessly with popular design tools like Sketch, Adobe XD, and Figma, allowing for smooth import and export of design files.',
+    
+    '8.User Testing and Research:InVision facilitates user testing and research, enabling teams to gather valuable insights and iterate on designs based on user feedback.',
+    
+    '9.Project Management:InVision provides project management features, allowing teams to organize and prioritize tasks, track progress, and meet deadlines effectively.',
+    
+   '10.Accessibility and Security:InVision prioritizes accessibility and security, ensuring that designs are accessible to all team members and protected from unauthorized access.',
+    
+    '11.Conclusion:In conclusion, InVision is a versatile platform that empowers teams to collaborate effectively throughout the design process. With its real-time collaboration, commenting and feedback, version control, prototyping, and integration with design tools, InVision is a valuable asset for any design team striving to deliver exceptional user experiences.',
+  
+  
+  ]
+  },
+]
 
 
 
