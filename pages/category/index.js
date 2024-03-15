39d5e1c801f63043926f926cc22cdfde58ef9085
sengkimin <sengin.pse@gmail.com>
2024-03-15 14:36:08 +0700

    category

---
+++ pages/category/index.js
@@ -0,0 +1,14 @@
+let categoryTitle = "Tutorial";//get from url
+
+const titleElem = document.getElementById("title");
+titleElem.innerText = categoryTitle;
+
+console.log(titleElem);
+
+let articles = [
+
+];
+
+// let displayArticles = filterByCategory(category);
+
+// displayToHtml(displayArticles);
