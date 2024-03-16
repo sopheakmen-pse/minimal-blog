e517035771391c5c058a88410bf90249c2406578
ThornThavann <thavann.thorn@institute.pse.ngo>
2024-03-16 10:13:21 +0700

    merge

--- pages/single/index.js
+++ pages/single/index.js
@@ -28,10 +28,30 @@ background:linear-gradient(60deg,red , yellow);
 color: transparent;
 text-align: center;
 font-family:sans-serif;
-font-size:65px;
+font-size:60px;
 margin-top:100px;
 ">10 hilarious NFT sales that <br> broke the internet</h1>
 
 
 <p style="text-align:  center; line-height: 40px; font-family:sans-serif; font-size:15px;">
 AUGUST 13, 2021 • <b> Daily digest </b> </p>`
+
+
+// document.getElementById("img-1") .innerHTML = `
+
+// `
+const imageUrls = [
+    "https://media.istockphoto.com/id/1304476690/photo/close-up-ux-developer-and-ui-designer-use-augmented-reality-app-brainstorming-about-mobile.webp?s=2048x2048&w=is&k=20&c=EAJpXnbHcKzP1AefO1xlzspgQgLAlN3oYEQJIAV8A4Y=",
+    "https://media.istockphoto.com/id/1205285884/photo/computer-hacker-coding-on-keyboard-on-a-background-of-monitors.webp?s=2048x2048&w=is&k=20&c=iNIa8lpBTdbtm9M0eQwe6kuRvk8oKhE8LFzCRIBywAg=",
+  ];
+ document.getElementById("img-1").innerHTML += `
+ <img src="${imageUrls[0]}"/>
+ `;
+ for (let i = 0; i < imageUrls.length;i++){
+    document.getElementById("img-1").innerHTML+= `
+ <img style="boder: 1px solid #ddd; padding:16px;" src="${imageUrls[i]}"/>
+ <p id="h">${imageUrls[1]}</p>
+ `;
+ 
+ }
+
