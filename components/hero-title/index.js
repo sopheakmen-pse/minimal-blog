3a13b528f41582e276674b02af324c58e67c22b9
sengkimer1 <kimer.seng@institute.pse.ngo>
2024-03-16 09:35:48 +0700

    done home page

--- components/hero-title/index.js
+++ components/hero-title/index.js
@@ -1,10 +1,10 @@
-document.querySelector(".container").innerHTML += `
-<div class="title">
+document.querySelector(".footer").innerHTML += `
+<div class="title-footer">
 <h1>Personally Newsletter</h1>
 <p>A bi-weekly newsletter of design inspiration, resources and anything related to career development.</p>
 </div>
 <div class="box">
        <input type="text" name="" id="" placeholder="Email address">
-        <button class="btn">Subscribe</button>
+        <button class="btn-sub">Subscribe</button>
 </div>
 `;
