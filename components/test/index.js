6e186648127b7bc3b2ae347f29e22e521b8426df
sreypheaphorn <sreyphea.phorn@institute.pse.ngo>
2024-03-18 13:28:17 +0700

    create single page a layout

---
+++ components/test/index.js
@@ -0,0 +1,67 @@
+// document.getElementById("paragraph") .innerHTML = `
+// <h1 id="title" style=" font-weight: bold; background-image: linear-gradient(60deg,#C41740,#EA9C28);  -webkit-background-clip :text;
+// color: transparent; text-align: center; font-family:sans-serif; font-size: 70px;"> 10 hilarious NFT sales that <br> broke the internet </h1>
+
+// <p style="text-align: center; font-family: sans-serif; font-size:25px; line-height: 1.5 ; color: #476457; margin-top:-20px">  AUGUST 13, 2021 • <b style="color:black;"> DAILY DIGEST <b> </p>
+// `
+
+  const imageUrls = [
+        "https://images.unsplash.com/photo-1468581264429-2548ef9eb732?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1497290756760-23ac55edf36f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
+  document.getElementById("image").innerHTML +=`
+  <img src= "${imageUrls[0]}" style="width: 1000px ; height: 750px; margin-left: 24%; border-radius: 10px; margin-top: 50px;"/>
+
+  <p style="font-size: 23px; width:70%; margin-left: 31%;  line-height: 1.5;">
+Design comps, layouts, wireframes—will your clients accept that you go about <br>
+ things  the facile way? Authorities in our business will tell in no uncertain terms <br>
+  that Lorem Ipsum is that huge, huge no no to forswear forever. <br> <br>
+
+Not so fast, I'd say, there are some redeeming factors in favor of greeking text, as <br> 
+its use is merely the symptom of a worse problem to take into consideration.<br> <br>
+
+The toppings you may chose for that TV dinner pizza slice when you forgot to <br> 
+ shop for foods, the paint you may slap on your face to impress the new boss is <br>
+  your business. But what about your daily bread? </p>
+
+  `;
+
+  document.getElementById("image").innerHTML +=`
+  <img src= "${imageUrls[1]}" style="width: 780px ; height: 490px; margin-left: 30%; border-radius: 10px; margin-top: 50px;"/>
+    
+  <p style="font-size:23px; width:70%; margin-left: 31%; line-height: 1.5;">
+
+The toppings you may chose for that TV dinner pizza slice when you forgot to <br>
+shop for foods, the paint you may slap on your face to impress the new boss is <br> 
+your business. But what about your daily bread? Not so fast, I'd say, there are <br>
+some redeeming factors in favor of greeking text, as its use is merely the <br>
+symptom of a worse problem to take into consideration. <br> <br>
+
+Design comps, layouts, wireframes—will your clients accept that you go about<br>
+things the facile way? Authorities in our business will tell in no uncertain terms<br>
+that Lorem Ipsum is that huge, huge no no to forswear forever. </p>
+
+  `;
+
+  document.getElementById("text").innerHTML =`
+  <h1 style="margin-left:29%; gap: 60%; border-left: 10px solid #C41740; font-size: 37px; padding-left: 20px;">
+  Design comps, layouts, wireframes-we belive <br>that clients will surely accept that you go about <br> things the facile way. It's a matter of time.
+  </h1>
+
+  <p style="font-size:23px; width: 70%; margin-left: 31%; margin-top: 50px; line-height: 1.5; ">
+
+  Design comps, layouts, wireframes—will your clients accept that you go about <br>
+  things the facile way? Authorities in our business will tell in no uncertain terms <br>
+  that Lorem Ipsum is that huge, huge no no to forswear forever.<br> <br> 
+
+  The toppings you may chose for that TV dinner pizza slice when you forgot to <br>
+  shop for foods, the paint you may slap on your face to impress the new boss is<br>
+  your business. But what about your daily bread? Not so fast, I'd say, there are <br>
+  some redeeming factors in favor of greeking text, as its use is merely the<br>
+  symptom of a worse problem to take into consideration. <br>  <br>
+
+  Not so fast, I'd say, there are some redeeming factors in favor of greeking text, as <br>
+  its use is merely the symptom of a worse problem to take into consideration. <br> <br>
+
+  Design comps, layouts, wireframes—will your clients accept that you go about <br>
+  things the facile way? Authorities in o </p>`;
